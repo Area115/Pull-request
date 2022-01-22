9678d61b179c7403fe8c88be76b08864896ed8b9
Omkar Suralkar <87975645+Area115@users.noreply.github.com>
2022-01-22 23:28:58 +0530

    i have made change in print line

--- Pull-request.py
+++ Pull-request.py
@@ -2,5 +2,5 @@ a = int(input("Enter the number 1 :  "))
 b = int(input("Enter the number 2 :  "))
 
 c = a+b 
-
+print(f"hello guys value of {a} + {b} is {a+b}")
 print(f"The value of {a} + {b}  is  =  {c}")
